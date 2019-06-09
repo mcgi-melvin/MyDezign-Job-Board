7d5ac9e1c6fef0a9f4c46c57465e7cfeb227871b
Melvin <melvinlomibao@gmail.com>
2019-06-10 00:37:42 +0800

    Update home.php

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
 }
 .loop-blog-single{
 	box-shadow: 0px 0px 1px #346672;
+	margin-bottom: 20px;
 }
 .loop-blog-body{
 	padding: 20px;
@@ -29,11 +30,13 @@
 $the_query = new WP_Query( array('order' => 'ASC' ) );
 if ( $the_query->have_posts() ) :
 	echo '<div class="row">';
+	echo '<div class="col-md-8 col-12">';
 	while ( $the_query->have_posts() ) :
 	$the_query->the_post();
 ?>
-	<div class="col-md-4 col-12">
+
 		<div class="loop-blog-single">
+			<a href="<?php echo get_permalink(); ?>">
 			<div class="featured-image">
 				<?php if(has_post_thumbnail()): ?>
 						<div style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>
@@ -41,20 +44,27 @@ if ( $the_query->have_posts() ) :
 						<div style="background-image: url('<?php echo site_url('/wp-content/uploads/woocommerce-placeholder.png'); ?>'); "></div>
 				<?php endif; ?>
 			</div>
+			</a>
 			<div class="loop-blog-body">
+				<a href="<?php echo get_permalink(); ?>">
 				<div class="title">
 					<h3><?php echo get_the_title(); ?></h3>
 				</div>
+				</a>
 				<div class="desc">
 					<p><?php echo get_the_excerpt(); ?></p>
 				</div>
 			</div>
 		</div>
-	</div>
+
 
 <?php
 	wp_reset_postdata();
 	endwhile;
+	echo '</div>
+	<div class="col-md-4 col-12">';
+	echo 	dynamic_sidebar('primary_sidebar');
+	echo '</div>';
 	echo '</div>'; //end row
 else:
 	echo 'No Post';
