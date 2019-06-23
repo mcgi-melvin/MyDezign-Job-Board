d3e0ea9cb2219e9be94b5fdb54554034ffeadeb3
Melvin <melvinlomibao@gmail.com>
2019-06-23 12:02:03 +0800

    Initial Commit

--- single-job_listing.php
+++ single-job_listing.php
@@ -12,13 +12,13 @@ if (have_posts()) : while (have_posts()) : the_post();
       <?php get_template_part('template-parts/desktop','ads'); ?>
     </div>
     <div class="row">
-      <div class="col-12 col-md-8 d-flex align-self-stretch">
+      <div class="col-12 col-md-12 col-lg-8 d-flex align-self-stretch">
         <div class="single-container">
           <h2><?php the_title(); ?></h2>
           <p><?php the_content(); ?></p>
         </div>
       </div>
-      <div class="col">
+      <div class="col-12 col-md-12 col-lg-4">
         <div class="single-sidebar">
           <?php dynamic_sidebar('single_job_list_1'); ?>
         </div>
